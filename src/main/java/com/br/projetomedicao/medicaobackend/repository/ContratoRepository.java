e89d347d8c36a84b95a11ca7bdac21aaa887fd1b
frettarenan <frettarenan@gmail.com>
2019-03-20 11:20:53 -0300

    implementação parcial da tela de pesquisa da matriz de medição

---
+++ src/main/java/com/br/projetomedicao/medicaobackend/repository/ContratoRepository.java
@@ -0,0 +1,14 @@
+package com.br.projetomedicao.medicaobackend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.projetomedicao.medicaobackend.model.Contrato;
+import com.br.projetomedicao.medicaobackend.model.Obra;
+
+public interface ContratoRepository extends JpaRepository<Contrato, Long> {
+
+	public List<Contrato> findByObra(Obra obra);
+	
+}
---
+++ src/main/java/com/br/projetomedicao/medicaobackend/resource/ContratoResource.java
@@ -0,0 +1,31 @@
+package com.br.projetomedicao.medicaobackend.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.projetomedicao.medicaobackend.model.Contrato;
+import com.br.projetomedicao.medicaobackend.model.Obra;
+import com.br.projetomedicao.medicaobackend.repository.ContratoRepository;
+
+@RestController
+@RequestMapping("/contratos")
+public class ContratoResource {
+
+	@Autowired
+	private ContratoRepository contratoRepository;
+	
+	@GetMapping("/status/ativo")
+	@PreAuthorize("isAuthenticated()")
+	public List<Contrato> listarStatusAtivoPorObra(@RequestParam(required = true) Long idObra) {
+		Obra obra = new Obra();
+		obra.setId(idObra);
+		return contratoRepository.findByObra(obra);
+	}
+
+}
