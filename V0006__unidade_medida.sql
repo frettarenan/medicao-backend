1754916f2bb62c425081b5749ea830328d25a005
frettarenan <frettarenan@gmail.com>
2019-03-23 21:05:09 -0300

    arquivo desnecessário

--- V0006__unidade_medida.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE medicao (
-	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(100) NOT NULL,
-	id_contrato BIGINT(20) NOT NULL,
-	FOREIGN KEY (id_contrato) REFERENCES contrato(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO contrato_medicao (nome, id_contrato) values ('Medição 1ª semana de Janeiro de 2019', 1);
