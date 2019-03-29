646944a2b6de7486f90b844839d18d31680d0eea
frettarenan <frettarenan@gmail.com>
2019-03-29 11:53:26 -0300

    Implementados métodos para o cadastro de contrato

--- src/main/java/com/br/projetomedicao/medicaobackend/resource/ContratoResource.java
+++ src/main/java/com/br/projetomedicao/medicaobackend/resource/ContratoResource.java
@@ -3,7 +3,11 @@ package com.br.projetomedicao.medicaobackend.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -12,6 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.projetomedicao.medicaobackend.event.RecursoCriadoEvent;
 import com.br.projetomedicao.medicaobackend.model.Contrato;
 import com.br.projetomedicao.medicaobackend.model.Obra;
 import com.br.projetomedicao.medicaobackend.repository.ContratoRepository;
@@ -34,17 +40,13 @@ public class ContratoResource {
 	@Autowired
 	private ContratoRepository contratoRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	@PreAuthorize("isAuthenticated()")
 	public Page<Contrato> pesquisar(@RequestParam(required = false) String numero, @RequestParam(required = false) String descricao, @RequestParam(required = false) Long idConstrutora, @RequestParam(required = false) Long idObra, Pageable pageable) {		
 		return contratoRepository.pesquisar(numero, descricao, idConstrutora, idObra, pageable);
-	}
-	
-	@PutMapping("/{id}/ativo")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_CONTRATO') and #oauth2.hasScope('write')")
-	public void atualizarPropriedadeAtivo(@PathVariable Long id, @RequestBody Boolean ativo) {
-		contratoService.atualizarPropriedadeAtivo(id, ativo);
 	}
 	
 	@DeleteMapping("/{id}")
@@ -54,11 +56,26 @@ public class ContratoResource {
 		contratoRepository.deleteById(id);
 	}
 	
+	@PostMapping
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_CONTRATO') and #oauth2.hasScope('write')")
+	public ResponseEntity<Contrato> criar(@Valid @RequestBody Contrato contrato, HttpServletResponse response) {
+		Contrato contratoBD = contratoService.salvar(contrato);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, contratoBD.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(contratoBD);
+	}
+	
 	@GetMapping("/{id}")
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CONTRATO') and #oauth2.hasScope('read')")
 	public ResponseEntity<Contrato> buscarPeloId(@PathVariable Long id) {
 		Optional<Contrato> contrato = contratoRepository.findById(id);
 		return contrato.isPresent() ? ResponseEntity.ok(contrato.get()) : ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/{id}")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_CONTRATO') and #oauth2.hasScope('write')")
+	public ResponseEntity<Contrato> atualizar(@PathVariable Long id, @Valid @RequestBody Contrato contrato) {
+		Contrato contratoBD = contratoService.atualizar(id, contrato);
+		return ResponseEntity.ok(contratoBD);
 	}
 	
 	@GetMapping("/status/ativo")
@@ -67,6 +84,13 @@ public class ContratoResource {
 		Obra obra = new Obra();
 		obra.setId(idObra);
 		return contratoRepository.findByAtivoAndObra(true, obra);
+	}
+	
+	@PutMapping("/{id}/ativo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_CONTRATO') and #oauth2.hasScope('write')")
+	public void atualizarPropriedadeAtivo(@PathVariable Long id, @RequestBody Boolean ativo) {
+		contratoService.atualizarPropriedadeAtivo(id, ativo);
 	}
 
 }
