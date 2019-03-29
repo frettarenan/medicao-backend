137b3542933ee36c94a2b9a7b7db68fefe521512
frettarenan <frettarenan@gmail.com>
2019-03-29 09:56:49 -0300

    Implementado get contrato by id

--- src/main/java/com/br/projetomedicao/medicaobackend/resource/ContratoResource.java
+++ src/main/java/com/br/projetomedicao/medicaobackend/resource/ContratoResource.java
@@ -1,11 +1,13 @@
 package com.br.projetomedicao.medicaobackend.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,6 +52,13 @@ public class ContratoResource {
 	@PreAuthorize("hasAuthority('ROLE_REMOVER_CONTRATO') and #oauth2.hasScope('write')")
 	public void remover(@PathVariable Long id) {
 		contratoRepository.deleteById(id);
+	}
+	
+	@GetMapping("/{id}")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_CONTRATO') and #oauth2.hasScope('read')")
+	public ResponseEntity<Contrato> buscarPeloId(@PathVariable Long id) {
+		Optional<Contrato> contrato = contratoRepository.findById(id);
+		return contrato.isPresent() ? ResponseEntity.ok(contrato.get()) : ResponseEntity.notFound().build();
 	}
 	
 	@GetMapping("/status/ativo")
