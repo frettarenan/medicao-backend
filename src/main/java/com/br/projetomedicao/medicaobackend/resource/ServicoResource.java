2e4c9b576776f12cb6fb0d39f8131bb17cfca9da
frettarenan <frettarenan@gmail.com>
2019-03-29 15:55:38 -0300

    Implementado recursos necessários para o cadastro de serviços

--- src/main/java/com/br/projetomedicao/medicaobackend/resource/ServicoResource.java
+++ src/main/java/com/br/projetomedicao/medicaobackend/resource/ServicoResource.java
@@ -2,17 +2,26 @@ package com.br.projetomedicao.medicaobackend.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.projetomedicao.medicaobackend.model.Contrato;
 import com.br.projetomedicao.medicaobackend.model.Medicao;
 import com.br.projetomedicao.medicaobackend.model.Servico;
 import com.br.projetomedicao.medicaobackend.repository.MedicaoRepository;
 import com.br.projetomedicao.medicaobackend.repository.ServicoRepository;
+import com.br.projetomedicao.medicaobackend.service.ServicoService;
 
 @RestController
 @RequestMapping("/servicos")
@@ -24,11 +33,33 @@ public class ServicoResource {
 	@Autowired
 	private MedicaoRepository medicaoRepository;
 	
-	@GetMapping
+	@Autowired
+	private ServicoService servicoService;
+	
+	@GetMapping("/contrato/{idContrato}")
 	@PreAuthorize("isAuthenticated()")
-	public List<Servico> listarGruposPorMedicao(@RequestParam(required = true) Long idMedicao) {
+	public List<Servico> listarTodosPorContrato(@PathVariable Long idContrato) {
+		Contrato contrato = new Contrato();
+		contrato.setId(idContrato);
+		return servicoRepository.findByContrato(contrato);
+	}
+	
+	@GetMapping("/medicao/{idMedicao}")
+	@PreAuthorize("isAuthenticated()")
+	public List<Servico> listarGruposPorMedicao(@PathVariable Long idMedicao) {
 		Medicao medicao = medicaoRepository.findById(idMedicao).get();
 		return servicoRepository.findByContrato(medicao.getContrato());
+	}
+	
+	@PostMapping("/cadastro-rapido")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_SERVICO') and #oauth2.hasScope('write')")
+	public ResponseEntity<List<Servico>> criarServicos(@Valid @RequestBody List<Servico> servicos, HttpServletResponse response) {
+		/*Optional<TipoGrupo> tipoGrupoCadastradoPeloUsuario = tipoGrupoRepository.findById(TipoGrupoEnum.CADASTRADO_PELO_USUARIO.getId());
+		for (Grupo grupo : grupos) {
+			grupo.setTipoGrupo(tipoGrupoCadastradoPeloUsuario.get());
+		}*/
+		List<Servico> servicosBD = servicoService.salvar(servicos);
+		return ResponseEntity.status(HttpStatus.CREATED).body(servicosBD);
 	}
 
 }
---
+++ src/main/java/com/br/projetomedicao/medicaobackend/service/ServicoService.java
@@ -0,0 +1,23 @@
+package com.br.projetomedicao.medicaobackend.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.projetomedicao.medicaobackend.model.Servico;
+import com.br.projetomedicao.medicaobackend.repository.ServicoRepository;
+
+@Service
+public class ServicoService {
+
+	@Autowired
+	private ServicoRepository servicoRepository;
+
+	public List<Servico> salvar(List<Servico> servicos) {
+		for (Servico servico : servicos) {
+			servicoRepository.save(servico);
+		}
+		return servicos;
+	}
+}
