1a7d98806b54611e3376a56b94a225cdc7d5fd50
frettarenan <frettarenan@gmail.com>
2019-04-04 10:53:23 -0300

    implementado mensagem informando se o login e senha estão inválidos

--- src/main/java/com/br/projetomedicao/medicaobackend/security/AppUserDetailsService.java
+++ src/main/java/com/br/projetomedicao/medicaobackend/security/AppUserDetailsService.java
@@ -25,7 +25,7 @@ public class AppUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
-		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
+		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário inexistente ou senha inválida"));
 		return new UsuarioSistema(usuario, getPermissoes(usuario));
 	}
 
