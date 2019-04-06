e2f8a4a5509ec2cfced5974c1bcbd94acfdae8bf
frettarenan <frettarenan@gmail.com>
2019-04-05 21:25:50 -0300

    removido import desnecessario

--- src/main/java/com/br/projetomedicao/medicaobackend/config/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/br/projetomedicao/medicaobackend/config/CustomResponseEntityExceptionHandler.java
@@ -14,7 +14,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
